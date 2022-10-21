c4b54ecd1da8d2a4d05e05b8ded342ebaee1876f
Kadah <Kadah.Coba@gmail.com>
2022-10-20 22:34:00 -0700

    Updated build script

--- kc_build.sh
+++ kc_build.sh
@@ -7,6 +7,8 @@ export AUTOBUILD_VARIABLES_FILE=$HOME/Dev/sl/fs-build-variables/variables
 
 autobuild installables edit fmodstudio platform=linux64 hash=f9bb7110e3ef4e7b50f41f1d54ba1d99 url=file:////opt/sl_3p/fmodstudio-2.02.09-linux64-222940517.tar.bz2
 
-time autobuild configure -A 64 -c ReleaseFS_open -- -DPACKAGE:BOOL=On --chan="KC" --fmodstudio -DUSE_SDL2:BOOL=On --clean
+CUDACXX=nvcc
 
-time autobuild build -A 64 -c ReleaseFS_open -- -DPACKAGE:BOOL=On --chan="KC" --fmodstudio -DUSE_SDL2:BOOL=On
+time autobuild configure -A 64 -c ReleaseFS_open -- -DPACKAGE:BOOL=On --chan="KC_CUDA" --fmodstudio  -DUSE_NVJPEG2K:BOOL=On -DUSE_SDL2:BOOL=On --clean
+
+time autobuild build -A 64 -c ReleaseFS_open -- -DPACKAGE:BOOL=On --chan="KC_CUDA" --fmodstudio  -DUSE_NVJPEG2K:BOOL=On -DUSE_SDL2:BOOL=On
