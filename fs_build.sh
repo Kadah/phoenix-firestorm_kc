7ba9ccef38b865dc3cbb52be0a29e76e29171876
Kadah <Kadah.Coba@gmail.com>
2022-10-20 22:28:05 -0700

    Updated build script

--- fs_build.sh
+++ fs_build.sh
@@ -5,8 +5,8 @@ conda activate sl
 
 export AUTOBUILD_VARIABLES_FILE=$HOME/Dev/sl/fs-build-variables/variables
 
-autobuild installables edit fmodstudio platform=linux64 hash=7248e66c779f26e54584a50f99a8b4c9 url=file:////nvme_mirror/Dev/sl/3p-fmodstudio/fmodstudio-2.02.07-linux64-222090151.tar.bz2
+autobuild installables edit fmodstudio platform=linux64 hash=f9bb7110e3ef4e7b50f41f1d54ba1d99 url=file:////opt/sl_3p/fmodstudio-2.02.09-linux64-222940517.tar.bz2
 
-time autobuild configure -A 64 -c ReleaseFS_open -- -DPACKAGE:BOOL=On --chan="KC" --fmodstudio --clean
+time autobuild configure -A 64 -c ReleaseFS_open -- -DPACKAGE:BOOL=On --chan="KC" --fmodstudio -DUSE_SDL2:BOOL=On --clean
 
-time autobuild build -A 64 -c ReleaseFS_open -- -DPACKAGE:BOOL=On --chan="KC" --fmodstudio
+time autobuild build -A 64 -c ReleaseFS_open -- -DPACKAGE:BOOL=On --chan="KC" --fmodstudio -DUSE_SDL2:BOOL=On
