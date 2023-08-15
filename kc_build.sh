a671df5fb6a2b21949d417c6344cd67b8c40d5ee
Kadah <Kadah.Coba@gmail.com>
2023-08-14 22:56:22 -0700

    Build script update

--- kc_build.sh
+++ kc_build.sh
@@ -5,7 +5,7 @@ conda activate sl
 
 export AUTOBUILD_VARIABLES_FILE=$HOME/Dev/sl/fs-build-variables/variables
 
-autobuild installables edit fmodstudio platform=linux64 hash=1e840d7e3d9a71f64031f77161e26605 url=file:////opt/sl_3p/fmodstudio-2.02.11-linux64-230210406.tar.bz2
+autobuild installables edit fmodstudio platform=linux64 hash=5734ad11b2c5079366e059797e09f57d url=file:////opt/sl_3p/fmodstudio-2.02.15-linux64-232270400.tar.bz2
 
 time autobuild configure -A 64 -c ReleaseFS_open -- -DPACKAGE:BOOL=On --chan="KC" --fmodstudio -DUSE_SDL2:BOOL=On --clean
 
